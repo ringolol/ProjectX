1e92c7e110cb0026e7e7c00684e16e797f277223
ringolol <57327672+ringolol@users.noreply.github.com>
2019-12-04 07:01:06 +0300

    Delete login_form.php

--- projectx_site/site/templates/login_form.php
+++
@@ -1,6 +0,0 @@
-<form class="box" action="" method="POST">
-	<h1>Login</h1>
-	<input type="text" name="login_field" placeholder="Username" required>
-	<input type="password" name="password_field" placeholder="Password" required>
-	<input type="submit" name="button_submit" value="Login">
-</form>
