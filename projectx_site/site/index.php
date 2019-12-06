8962a28bb18db848529a299f2718db2d2f058708
ringolol <57327672+ringolol@users.noreply.github.com>
2019-12-06 18:55:21 +0300

    clean_up_and_upd_login_form

--- projectx_site/site/index.php
+++ projectx_site/site/index.php
@@ -2,15 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="login-form/login-form.css">
+    <link rel="preload" href="img/login-form-background.webp" as="image" type="image/webp"  >
     <title>Login</title>
 </head>
-<body>
-    <form class="box" action="login.php" method="post">
-        <h1>Login</h1>
-        <input type="text" name="login_field" placeholder="Username" required>
-        <input type="password" name="password_field" placeholder="Password" required>
-        <input type="submit" name="login_button" value="Login">
+<body class="body">
+    <form class="login-form" action="login.php" method="post">
+        <h1 class="login-form__title-text">Login</h1>
+        <input class="input login-form__input" type="text" name="login_field" placeholder="Username" style="background:#0a0e31;" required>
+        <input class="input login-form__input" type="password" name="password_field" placeholder="Password" style="background:#0a0e31;" required>
+        <input class="button login-form__button" type="submit" name="login_button" value="Login">
     </form>
 </body>
 </html>
