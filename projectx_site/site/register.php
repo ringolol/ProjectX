2fb73eda52c59feb66cb44fc2df9120a684f2fbb
ringolol <57327672+ringolol@users.noreply.github.com>
2019-12-10 18:10:45 +0300

    add_site_structure

--- projectx_site/site/register.php
+++ projectx_site/site/register.php
@@ -1,105 +1,101 @@
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-  header("location: index.php");
-  exit;
-}
+    
+    // Check if the user is already logged in, if yes then redirect him to welcome page
+    require 'web_scripts/is_logged_in.php';
 
-// Include config file
-require_once "config.php";
- 
-// Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT user_id FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+    // Connect db 
+    require_once "config_db.php";
+    
+    // Define variables and initialize with empty values
+    $username = $password = $confirm_password = "";
+    $username_err = $password_err = $confirm_password_err = "";
+    
+    // Processing form data when form is submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+        // Validate username
+        if(empty(trim($_POST["username"]))){
+            $username_err = "Please enter a username.";
+        } else{
+            // Prepare a select statement
+            $sql = "SELECT user_id FROM users WHERE username = ?";
             
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
+            if($stmt = mysqli_prepare($con, $sql)){
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "s", $param_username);
                 
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
+                // Set parameters
+                $param_username = trim($_POST["username"]);
+                
+                // Attempt to execute the prepared statement
+                if(mysqli_stmt_execute($stmt)){
+                    /* store result */
+                    mysqli_stmt_store_result($stmt);
+                    
+                    if(mysqli_stmt_num_rows($stmt) == 1){
+                        $username_err = "This username is already taken.";
+                    } else{
+                        $username = trim($_POST["username"]);
+                    }
                 } else{
-                    $username = trim($_POST["username"]);
+                    echo "Oops! Something went wrong. Please try again later.";
                 }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
             }
+            
+            // Close statement
+            mysqli_stmt_close($stmt);
         }
-         
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
-    
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-    
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-         
-        if($stmt = mysqli_prepare($con, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-            
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: login.php");
-            } else{
-                echo "Something went wrong. Please try again later.";
+        // Validate password
+        if(empty(trim($_POST["password"]))){
+            $password_err = "Please enter a password.";     
+        } elseif(strlen(trim($_POST["password"])) < 6){
+            $password_err = "Password must have atleast 6 characters.";
+        } else{
+            $password = trim($_POST["password"]);
+        }
+        
+        // Validate confirm password
+        if(empty(trim($_POST["confirm_password"]))){
+            $confirm_password_err = "Please confirm password.";     
+        } else{
+            $confirm_password = trim($_POST["confirm_password"]);
+            if(empty($password_err) && ($password != $confirm_password)){
+                $confirm_password_err = "Password did not match.";
             }
         }
-         
-        // Close statement
-        mysqli_stmt_close($stmt);
+        
+        // Check input errors before inserting in database
+        if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+            
+            // Prepare an insert statement
+            $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+            
+            if($stmt = mysqli_prepare($con, $sql)){
+                // Bind variables to the prepared statement as parameters
+                mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+                
+                // Set parameters
+                $param_username = $username;
+                // Creates a password hash
+                $param_password = password_hash($password, PASSWORD_DEFAULT); 
+                
+                // Attempt to execute the prepared statement
+                if(mysqli_stmt_execute($stmt)){
+                    // Redirect to login page
+                    header("location: login.php");
+                } else{
+                    echo "Something went wrong 2. Please try again later.";
+                }
+            }
+            
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+        
+        // Close connection
+        mysqli_close($con);
     }
-    
-    // Close connection
-    mysqli_close($con);
-}
 ?>
  
 <!DOCTYPE html>
@@ -112,7 +108,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 
 <body class="body">
-    <form class="login-form" action="register.php" method="post">
+    <form class="login-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <h1 class="login-form__title-text">Sign Up</h1>
         <input class="input login-form__input" type="text" name="username" placeholder="Username" style="background:#0a0e31;" required>
         <input class="input login-form__input" type="password" name="password" placeholder="Password" style="background:#0a0e31;" required>
