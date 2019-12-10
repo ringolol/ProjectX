884b044a2e37668e40182a9a82766f7266699a89
ReSenpai <bezbana1@gmail.com>
2019-12-10 20:30:08 +0300

    Fix login form css

--- projectx_site/site/login.php
+++ projectx_site/site/login.php
@@ -91,8 +91,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="login-form/login-form.css">
-    <link rel="preload" href="img/login-form-background.webp" as="image" type="image/webp">
+    <link rel="stylesheet" href="css/login-form.css">
+    <link rel="preload" href="css" as="style" type="css/vars.css">
+    <link rel="preload" href="css" as="style" type="css/index.css">
 </head>
 
 <body>
